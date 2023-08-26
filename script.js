80358099f0f3921d2ef6231c2f04eb76a917f7f9
Henderson Sousa <128432655+HendersonSousa20@users.noreply.github.com>
2023-08-26 10:33:07 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+
+var botao = document.getElementById("btn");
+
+// Adiciona um ouvinte de evento para o clique no botão
+botao.addEventListener("click", function() {
+  alert("E-mail enviado com sucesso");
+});
